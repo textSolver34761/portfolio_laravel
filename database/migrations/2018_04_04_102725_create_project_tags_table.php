4752b93dfb80ec56e6f3a071f70cb2fbbf7efd04
textSolver34761 <pradonbenjamin@gmail.com>
2018-04-24 10:56:06 +0200

    creation de 2 tables pivots

--- database/migrations/2018_04_04_102725_create_project_tags_table.php
+++ database/migrations/2018_04_04_102725_create_project_tags_table.php
@@ -13,14 +13,14 @@ class CreateProjectTagsTable extends Migration
      */
     public function up()
     {
-        Schema::create('project_tags', function (Blueprint $table) {
+        Schema::create('project_tag', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('projects_id')->index();
             $table->unsignedInteger('tags_id')->index();
             $table->engine = 'InnoDB';
         });
 
-        Schema::table('project_tags', function($table){            
+        Schema::table('project_tag', function($table){            
             $table->foreign('projects_id')->references('id')->on('projects');
             $table->foreign('tags_id')->references('id')->on('tags');
         });
@@ -33,6 +33,6 @@ class CreateProjectTagsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('project_tags');
+        Schema::dropIfExists('project_tag');
     }
 }
