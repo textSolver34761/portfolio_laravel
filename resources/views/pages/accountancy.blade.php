bac4898f60293a96f3f87646c9540c728dd19382
textSolver34761 <pradonbenjamin@gmail.com>
2018-07-06 17:51:52 +0200

    update budget part

---
+++ resources/views/pages/accountancy.blade.php
@@ -0,0 +1,146 @@
+<h1 class="text-center">Accountancy</h1>
+<div class="tableaux">
+    <table>
+            <tr>
+                <th>
+                    Idea 1
+                </th>
+            </tr>
+        <tr>
+            <th>Months</th>
+            <th>Earnings</th>
+        </tr>
+
+        <tr>
+            <td>January 2018</td>
+            <td>0,00€</td>
+        </tr>
+        
+        <tr>
+            <td>February 2018</td>
+            <td>0,00€</td>
+        </tr>
+
+        <tr>
+            <td>March 2018</td>
+            <td>0,00€</td>
+        </tr>
+        
+        <tr>
+            <td>April 2018</td>
+            <td>0,00€</td>
+        </tr>
+
+        <tr>
+            <td>May 2018</td>
+            <td>0,00€</td>
+        </tr>
+
+        <tr>
+            <td>June 2018</td>
+            <td>0,00€</td>
+        </tr>
+
+        <tr>
+            <td>Jully 2018</td>
+            <td>0,00€</td>
+        </tr>
+
+        <tr>
+            <td>August 2018</td>
+            <td>0,00€</td>
+        </tr>
+
+            <tr>
+                <td>Sept. 2018</td>
+                <td>0,00€</td>
+            </tr>
+
+            <tr>
+                <td>Nov. 2018</td>
+                <td>0,00€</td>
+            </tr>
+
+            <tr>
+                <td>Dece. 2018</td>
+                <td>0,00€</td>
+            </tr> 
+    </table>
+</div>
+<div class="tableaux">
+    <table>
+            <tr>
+                <th>
+                    Idea 2
+                </th>
+            </tr>
+        <tr>
+            <th>Months</th>
+            <th>Earnings</th>
+        </tr>
+
+        <tr>
+            <td>January 2018</td>
+            <td>0,00€</td>
+        </tr>
+        
+        <tr>
+            <td>February 2018</td>
+            <td>0,00€</td>
+        </tr>
+
+        <tr>
+            <td>March 2018</td>
+            <td>0,00€</td>
+        </tr>
+        
+        <tr>
+            <td>April 2018</td>
+            <td>0,00€</td>
+        </tr>
+
+        <tr>
+            <td>May 2018</td>
+            <td>0,00€</td>
+        </tr>
+
+        <tr>
+            <td>June 2018</td>
+            <td>0,00€</td>
+        </tr>
+
+        <tr>
+            <td>Jully 2018</td>
+            <td>0,00€</td>
+        </tr>
+
+        <tr>
+            <td>August 2018</td>
+            <td>0,00€</td>
+        </tr>
+
+            <tr>
+                <td>Sept. 2018</td>
+                <td>0,00€</td>
+            </tr>
+
+            <tr>
+                <td>Nov. 2018</td>
+                <td>0,00€</td>
+            </tr>
+
+            <tr>
+                <td>Dece. 2018</td>
+                <td>0,00€</td>
+            </tr> 
+    </table>
+</div>
+<style>
+table, th, td {
+    border: 1px solid black;
+}
+.tableaux{
+    display:inline-block;
+    vertical-align:top;
+  }
+</style>
